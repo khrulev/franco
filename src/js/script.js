2d2c5b73ca3ffc5b32190234767e30833e65beb3
Ivan Khrulev <subz07@bk.ru>
2020-07-27 22:54:56 +0500

    add section(FEATURED ITEMS)

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+
+@@include('./slick.min.js');
+
 $(document).ready(function(){ 
     
     $('.menu__burger').on('click',function(e){
@@ -9,20 +12,38 @@ $(document).ready(function(){
         $('.menu__burger, .menu__body').removeClass('active');
         $('body').removeClass('lock');})
         
-    // $('.slider').slick(
-    //     {arrows: false, dots: true, fade: true}
-    // );
+    $('.slider').slick(
+        {
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        appendArrows: $('.slider-arrow'),
+        prevArrow: '<button id="prev" type="button" class="slick-prev slick-arrow"><img src="../img/main/slider/left-arrow.png"></img></button>',
+        nextArrow: '<button id="next" type="button" class="slick-next slick-arrow"><img src="../img/main/slider/right-arrow.png"></img></button>'
+        }
+    );
 
     window.onscroll = function() {myFunction()};
 
     var navbar = document.getElementById("navbar");
+    var jumbotron = document.getElementById("jumbotron");
     var sticky = navbar.offsetTop;
     
     function myFunction() {
-    if (window.pageYOffset > sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
+        if (window.pageYOffset > sticky) {
+            navbar.classList.add("sticky");
+            jumbotron.classList.add("sticky_jumbo");
+            
+        } else {
+            navbar.classList.remove("sticky");
+            jumbotron.classList.remove("sticky_jumbo");
+        }
     }
-    }
-})
+    
+});
+
+
+
+
+
